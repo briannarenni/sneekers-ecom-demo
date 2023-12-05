6b0fc65979ae80e85a359287a913e98f5800b116
Brianna Renni <briannaxrene@icloud.com>
2023-12-05 04:54:48 -0800

    feat: Add component files

---
+++ src/lib/components/ProductInfo.svelte
@@ -0,0 +1,21 @@
+<script></script>
+
+<div class="product-info">
+  <p>#sweaterweather</p>
+
+  <h2>Fall Limited Edition Sneakers</h2>
+
+  <p>
+    These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber
+    outer sole, they’ll withstand everything the weather can offer.
+  </p>
+
+  <h2>$125.00</h2>
+  <p>50%</p>
+  <p>$250.00</p>
+
+  <input type="range" />0
+  <button class="add-cart-btn">Add to cart</button>
+</div>
+
+<style></style>
---
+++ src/lib/components/nav/MobileNav.svelte
@@ -0,0 +1,35 @@
+<script>
+  const handleMenuClick = () => {
+    event.preventDefault();
+  };
+</script>
+
+<nav class="menu left drawer min">
+  <!-- TODO: Close button -->
+  <a href="/" on:click={handleMenuClick}>
+    <!-- TODO: Icon -->
+    <div>Collections</div>
+  </a>
+
+  <a href="/" on:click={handleMenuClick}>
+    <!-- TODO: Icon -->
+    <div>Men</div>
+  </a>
+
+  <a href="/" on:click={handleMenuClick}>
+    <!-- TODO: Icon -->
+    <div>Women</div>
+  </a>
+
+  <a href="/" on:click={handleMenuClick}>
+    <!-- TODO: Icon -->
+    <div>About</div>
+  </a>
+
+  <a href="/" on:click={handleMenuClick}>
+    <!-- TODO: Icon -->
+    <div>Contact</div>
+  </a>
+</nav>
+
+<style></style>
