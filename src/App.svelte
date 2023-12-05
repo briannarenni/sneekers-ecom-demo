8748790af732bbec972d28787257e512ef5e32ec
Brianna Renni <briannaxrene@icloud.com>
2023-12-05 04:54:20 -0800

    style: Customize Beer theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,10 @@
 <script>
-
 </script>
 
 <main>
-  Collections
-  Men
-  Women
-  About
-  Contact
 
-  #sweaterweather
 
-  Fall Limited Edition Sneakers
-
-  These low-profile sneakers are your perfect casual wear companion. Featuring a
-  durable rubber outer sole, they’ll withstand everything the weather can offer.
-
-  $125.00
-  50%
-  $250.00
-
-  0
-  Add to cart
 </main>
 
 <style>
-
 </style>
